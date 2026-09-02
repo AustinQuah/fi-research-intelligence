15dfd55c67a496823c8819c7a2d157e59091f5cb
AustinQuah <austinquah@gmail.com>
2026-09-02 17:14:07 +0800

    Change import path for App component to App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,21 +4,15 @@ import {
   createRoot
 } from "react-dom/client";
 
-import App from "./App";
+import App from "./App.jsx";
 
 import "./styles.css";
 
 
 createRoot(
-  document.getElementById(
-    "root"
-  )
+  document.getElementById("root")
 ).render(
-
   <React.StrictMode>
-
     <App />
-
   </React.StrictMode>
-
 );
